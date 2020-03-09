6fb14c544ffafc9f46b9b32393af64b5cb0b644e
flanker <flanker@bk.ru>
2020-03-09 17:59:33 +0300

    *

--- chroot/udpxy/install.sh
+++ chroot/udpxy/install.sh
@@ -9,16 +9,9 @@
 # http://mybooklive:4022/restart
 ###
 
-
-mkdir $(dirname $0)/install
-cd $(dirname $0)/install
-tar -xvf ../udpxy-ppc_mbl.tar
-chmod 755 udpxy
-chmod 755 udpxrec
-chmod 755 udpxy.sh
-mv udpxy $1/usr/bin/udpxy
-mv udpxrec $1/usr/bin/udpxrec
-mv udpxy.sh $1/etc/init.d/udpxy
-cd
-rm -R $(dirname $0)/install
-chroot $1 update-rc.d -f udpxy defaults 40
+cp $(dirname $0)/udpxy-ppc_mbl/udpxy $1/usr/bin/udpxy
+cp $(dirname $0)/udpxy-ppc_mbl/udpxrec $1/usr/bin/udpxrec
+cp $(dirname $0)/udpxy-ppc_mbl/udpxy.sh $1/etc/init.d/udpxy
+chmod 755 $1/usr/bin/udpxy
+chmod 755 $1/usr/bin/udpxrec
+chmod 755 $1/etc/init.d/udpxy
